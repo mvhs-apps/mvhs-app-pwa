20db32a155e75d32799434a997dc6c69c4e725fb
plusCubed <plusCubed@users.noreply.github.com>
2017-08-08 13:17:05 -0700

    Add icons

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -310,7 +310,9 @@ module.exports = {
       externals: [
         '/manifest.json',
         '/icons/android-chrome-192x192.png',
-        '/icons/android-chrome-512x512.png'
+        '/icons/android-chrome-512x512.png',
+        '/icons/apple-touch-icon.png',
+        '/icons/safari-pinned-tab.svg'
       ],
       AppCache: {
         FALLBACK: {'/': '/'}
