d5d62c0071fe52d9ddd5de2c858fa7ce2c13ebed
Marc Bacvanski <marc.bacvanski@gmail.com>
2017-08-12 16:54:24 -0700

    Repair webpack config after sketchy refactoring

--- web/config/webpack.config.prod.js
+++ web/config/webpack.config.prod.js
@@ -154,7 +154,7 @@ module.exports = {
       // assets smaller than specified size as data URLs to avoid requests.
       {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('calendarURL-loader'),
+        loader: require.resolve('url-loader'),
         options: {
           limit: 10000,
           name: 'static/media/[name].[hash:8].[ext]'
