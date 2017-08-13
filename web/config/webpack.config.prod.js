149373e48ef34ea4b9a86415b14e697ba64d85ff
plusCubed <plusCubed@users.noreply.github.com>
2017-08-13 15:04:50 -0700

    Add eslint path to webpack config

--- web/config/webpack.config.prod.js
+++ web/config/webpack.config.prod.js
@@ -119,7 +119,8 @@ module.exports = {
         use: [
           {
             options: {
-              formatter: eslintFormatter
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint')
             },
             loader: require.resolve('eslint-loader')
           }
