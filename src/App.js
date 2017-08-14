a2a887603e79ff780ae4e7fb688afb155aa2e5c6
RonArel <RonusTheBoss123@gmail.com>
2017-08-13 20:07:56 -0700

    Credit Page
    Credits Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotificationIcon from 'material-ui-icons/Notifications';
 import MapIcon from 'material-ui-icons/Map';
 import SearchIcon from 'material-ui-icons/Search';
 import AppBar from 'material-ui/AppBar';
+import InfoIcon from 'material-ui-icons/Info';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Tabs, { Tab } from 'material-ui/Tabs';
@@ -19,6 +20,7 @@ import amber from 'material-ui/colors/amber';
 import SchedulePageContainer from './containers/SchedulePageContainer';
 import Map from './components/map';
 import Search from './components/search';
+import Credit from './components/Credit';
 
 import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import type { RouterHistory } from 'react-router-dom';
@@ -48,7 +50,7 @@ const RouterTabs = withRouter(
   }
 );
 
-const routes = ['/', '/map', '/search'];
+const routes = ['/', '/map', '/search', '/Credit'];
 
 const theme = createMuiTheme({
   palette: createPalette({
@@ -72,12 +74,14 @@ const App = () => {
               <LinkTab icon={<NotificationIcon />} to={routes[0]} />
               <LinkTab icon={<MapIcon />} to={routes[1]} />
               <LinkTab icon={<SearchIcon />} to={routes[2]} />
+              <LinkTab icon={<InfoIcon />} to={routes[3]} />
             </RouterTabs>
           </AppBar>
 
           <Route exact path={routes[0]} component={SchedulePageContainer} />
           <Route path={routes[1]} component={Map} />
           <Route path={routes[2]} component={Search} />
+          <Route path={routes[3]} component={Credit} />
         </div>
       </Router>
     </MuiThemeProvider>
---
+++ src/components/Credit.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import * as React from 'react';
+
+class Credit extends Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default Credit;
