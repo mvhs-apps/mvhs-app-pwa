2ca6f0cc184eaa5f72e415cc492c9169313d2b1e
plusCubed <plusCubed@users.noreply.github.com>
2017-08-17 23:03:53 -0700

    Add mock data to layout, extract CSS, add actual credits data

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -1,157 +1,28 @@
-//@ flow weak
-import { Component } from 'react';
-import * as React from 'react';
+//@ flow
+
+import React from 'react';
+
 import './Credit.css';
 
 import PropTypes from 'prop-types';
-import { withStyles, createStyleSheet } from 'material-ui/styles';
+import { withStyles } from 'material-ui/styles';
 import Card, { CardActions, CardContent } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 
-const styleSheet = createStyleSheet(theme => ({
-  card: {
-    minWidth: 275
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  title: {
-    marginBottom: 16,
-    fontSize: 14,
-    color: theme.palette.text.secondary
-  },
-  pos: {
-    marginBottom: 12,
-    color: theme.palette.text.secondary
-  }
-}));
-
-function SimpleCard(props) {
-  const classes = props.classes;
-  const bull = <span className={classes.bullet}>•</span>;
-
+const SimpleCard = props => {
   return (
-    <div>
-      <Card className={classes.card}>
+    <div className="about">
+      <Card>
         <CardContent>
-          {/* <CardMedia> */}
-          {/* <img src={reptileImage} alt="Contemplative Reptile" /> */}
-          {/* </CardMedia> */}
-          <Typography type="body1" className={classes.title}>
-            Role 1
-          </Typography>
           <Typography type="headline" component="h2">
-            Name
+            Daniel Ciao
           </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
+          <Typography type="body1" className="about-subtitle">
+            Lead Developer
           </Typography>
           <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="body1" className={classes.title}>
-            Role 2
-          </Typography>
-          <Typography type="headline" component="h2">
-            Name
-          </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="body1" className={classes.title}>
-            Role 3
-          </Typography>
-          <Typography type="headline" component="h2">
-            Name
-          </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="body1" className={classes.title}>
-            Role 4
-          </Typography>
-          <Typography type="headline" component="h2">
-            Name
-          </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="body1" className={classes.title}>
-            Role 5
-          </Typography>
-          <Typography type="headline" component="h2">
-            Name
-          </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button dense>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="body1" className={classes.title}>
-            Role 6
-          </Typography>
-          <Typography type="headline" component="h2">
-            Name
-          </Typography>
-          <Typography type="body1" className={classes.pos}>
-            adjective
-          </Typography>
-          <Typography component="p">
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
+            Overall app framework and design
           </Typography>
         </CardContent>
         <CardActions>
@@ -160,10 +31,6 @@ function SimpleCard(props) {
       </Card>
     </div>
   );
-}
-
-SimpleCard.propTypes = {
-  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styleSheet)(SimpleCard);
+export default SimpleCard;
