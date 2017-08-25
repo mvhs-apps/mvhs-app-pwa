f008b88916247dd87aad634ceb661bcb6d9205ac
plusCubed <plusCubed@users.noreply.github.com>
2017-08-24 19:06:25 -0700

    Cache calendar requests using service worker

--- src/index.js
+++ src/index.js
@@ -5,23 +5,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import registerServiceWorker from './registerServiceWorker';
 
 import './utils/addtohomescreen.js';
 import './utils/addtohomescreen.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-OfflinePluginRuntime.install({
-  onUpdateReady: () => {
-    console.log('SW Event:', 'onUpdateReady');
-    OfflinePluginRuntime.applyUpdate();
-  },
-  onUpdated: () => {
-    console.log('SW Event:', 'onUpdated');
-    // TODO: Use snackbar for proper updating
-    window.location.reload();
-  }
-});
+registerServiceWorker();
+if (!('serviceWorker' in navigator)) OfflinePluginRuntime.install();
 
 window.addToHomescreen({
   lifespan: 0,
