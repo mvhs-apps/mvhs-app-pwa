cf86494c92681dc02995481e32b9688962c5d25f
plusCubed <pluscubed@users.noreply.github.com>
2017-08-27 22:11:26 -0700

    Add link to disclaimer

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -2,10 +2,21 @@
 
 import React from 'react';
 
-import disclaimer from '../assets/disclaimer.json';
-
 import './Disclaimer.css';
 
-const Disclaimer = () => <div className="disclaimer">{disclaimer}</div>;
+const Disclaimer = () => (
+  <div className="disclaimer">
+    The information may not be fully accurate or finalized due to various
+    unforeseen reasons. It is important to double check with the{' '}
+    <a
+      href="http://www.mvla.net/mvhs"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      school website
+    </a>{' '}
+    for official school-related information.
+  </div>
+);
 
 export default Disclaimer;
