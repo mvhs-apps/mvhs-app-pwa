0ed347cc59102be4d62449a66d78e9427f1853b4
plusCubed <pluscubed@users.noreply.github.com>
2017-09-05 20:51:56 -0700

    Add GitHub link and club description to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,27 +1,41 @@
 //@ flow
 
-import React from 'react';
+import * as React from 'react';
 import CreditsContainer from '../containers/CreditsContainer';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 
 import './AboutPage.css';
 
 const AboutPage = () => {
   return (
     <div>
-      <div className="about-club">
+      <div className="about-club center">
         <Typography type="title" align="center" className="about-club-title">
           MVHS Computer Science Club
         </Typography>
-        <Typography type="subheading" align="center">
-          <a
-            href="http://club.mvhs.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            club.mvhs.io
-          </a>
+        <Typography type="body1" className="about-club-desc balance-text">
+          Join us to help develop this app and others for the MVHS community! No
+          programming experience needed, meetings Tuesday at lunch in Room 522.
         </Typography>
+        <Button
+          className=""
+          href="http://club.mvhs.io"
+          target="_blank"
+          rel="noopener noreferrer"
+          color="accent"
+        >
+          club.mvhs.io
+        </Button>
+        <Button
+          className="center"
+          href="https://github.com/mvhs-apps/mvhs-app-pwa"
+          target="_blank"
+          rel="noopener noreferrer"
+          color="accent"
+        >
+          GitHub
+        </Button>
       </div>
       <CreditsContainer />
     </div>
