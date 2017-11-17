b55d7d6db2eb5f51091546ac40381711e0bee218
John Park <KronosOfWarTime@gmail.com>
2017-11-16 16:42:42 -0800

    Added fetch for Search
    Now fetches from firebase and tests it instead of the local json file

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,161 +2,95 @@ import React from 'react';
 import { Component } from 'react';
 import map from '../assets/schoolmap.svg';
 import './Map.css';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+
+//console.log(data);
+
+var finalarray = [];
+
+var data;
+
+//alert(data.length);
 
 class Map extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
-    this.state = { name: 'boverlay' };
-    this.state = { fname: 'boverlay' };
-    this.state = { aname: 'boverlay' };
-    this.state = { lname: 'boverlay' };
-    this.state = { cname: 'boverlay' };
-    this.state = { rname: 'boverlay' };
+    this.state = { value: '' }; //for searchbar
+
+    this.state = { loading: true };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-  handleSubmit(event) {
-    var adminbuilding = [
-      'activities',
-      'office',
-      'chac',
-      'clubs',
-      'confiscated',
-      'phones',
-      'lost',
-      'found',
-      'principal',
-      'principals',
-      'drug',
-      'grissom',
-      'teacher',
-      'teachers',
-      'mail',
-      'theft',
-      'report',
-      'vice',
-      'work',
-      'permits'
-    ];
-    var financebuilding = [
-      'money',
-      'check',
-      'bus',
-      'pass',
-      'buy',
-      'tickets',
-      'clothes',
-      'dance',
-      'replacement',
-      'card',
-      'locker',
-      'parking',
-      'permit',
-      'reduced',
-      'lunch',
-      'finance'
-    ];
-    var attendanceoffice = ['tardy', 'late'];
-    var library = ['print schedule', 'book', 'print'];
-    var counselingoffices = [
-      'counseling',
-      'counselor',
-      'appointment',
-      'mental',
-      'schedule'
-    ];
-    var registar = ['grade', 'transcript'];
-    alert('A query was submitted: ' + this.state.value);
-    event.preventDefault();
-    var phrase = /*toLowerCase(*/ this.state.value; //);
-    //check adminbuilding
-    var somethingfound = false;
-    for (var i = 0; i < adminbuilding.length - 1; i++) {
-      if (phrase.includes(adminbuilding[i])) {
-        alert('adminbuilding');
-        i = adminbuilding.length - 1;
-        somethingfound = true;
-        this.setState({ name: 'overlay4admin' });
-      }
-    }
-    //check Finance
-    for (var i = 0; i < financebuilding.length - 1; i++) {
-      if (phrase.includes(financebuilding[i])) {
-        alert('finance');
-        i = financebuilding.length - 1;
-        somethingfound = true;
-        this.setState({ name: 'overlay4finance' });
-      }
-    }
-    //check attendance
-    for (var i = 0; i < attendanceoffice.length - 1; i++) {
-      if (phrase.includes(attendanceoffice[i])) {
-        alert('attendanceoffice');
-        i = attendanceoffice.length - 1;
-        somethingfound = true;
-        this.setState({ fname: 'overlay4attendance' });
-      }
-    }
-    //check Library
-    for (var i = 0; i < library.length - 1; i++) {
-      if (phrase.includes(library[i])) {
-        alert('library');
-        i = library.length - 1;
-        somethingfound = true;
-        this.setState({ lname: 'overlay4library' });
-      }
-    }
-    //counseling offices
-    for (var i = 0; i < counselingoffices.length - 1; i++) {
-      if (phrase.includes(counselingoffices[i])) {
-        alert('counselingoffices');
-        i = counselingoffices.length - 1;
-        somethingfound = true;
-        this.setState({ cname: 'overlay4conseling' });
-      }
-    }
-    //registar
-    for (var i = 0; i < registar.length - 1; i++) {
-      if (phrase.includes(registar[i])) {
-        alert('registar');
-        i = registar.length - 1;
-        somethingfound = true;
-      }
-    }
+  componentDidMount() {
+    //json variable
+    var url = 'https://mvhs-app-d04d2.firebaseio.com/locations.json';
 
-    if (!somethingfound) {
-      alert('!somethingfound');
+    //Use fetch to get the spreadsheet data
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonData => {
+        //add the jsonData to a state variable
+        if (jsonData != null) {
+          data = jsonData;
+          this.setState({ loading: false });
+          console.log(this.state.loading);
+          console.log(data);
+        }
+      });
+  }
+
+  handleChange(event) {
+    //alert("IN CHANGE");
+    console.log(event.target.value);
+
+    finalarray = [];
+    this.setState({ value: event.target.value });
+
+    for (var i = 0; i < data.length; i++) {
+      for (var j = 0; j < data[i].KeyWords.length; j++) {
+        console.log(
+          data[i].name + ' ' + data[i].KeyWords[j] + ' ' + event.target.value
+        );
+        if (event.target.value.includes(data[i].KeyWords[j].toLowerCase())) {
+          console.log('something found!!');
+          console.log(data[i].Location);
+
+          console.log('FOUND!!!!!' + data[i].Location);
+
+          finalarray.push(<li>{data[i].Location}</li>);
+          console.log(finalarray);
+        }
+      }
     }
+    console.log('');
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Search:
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-
-        <div className={this.state.name} />
-        <div className={this.state.fname} />
-        <div className={this.state.lname} />
-        <div className={this.state.aname} />
         <div className="map-container">
           <img alt="map" className="map" src={map} />
         </div>
+
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            label="Search"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </form>
+        <ul>{finalarray}</ul>
       </div>
     );
   }
