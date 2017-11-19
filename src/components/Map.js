831dfa2e49cd834232715feedf18dba832e2d2c0
John Park <KronosOfWarTime@gmail.com>
2017-11-19 10:41:16 -0800

    Better Search
    Now will search at 3 letters

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,6 +7,7 @@ import TextField from 'material-ui/TextField';
 //console.log(data);
 
 var finalarray = [];
+var checkarray = [];
 
 var data;
 
@@ -46,25 +47,66 @@ class Map extends Component {
     console.log(event.target.value);
 
     finalarray = [];
+    checkarray = [];
+    var first = true;
     this.setState({ value: event.target.value });
+    if (event.target.value.length > 2) {
+      for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].KeyWords.length; j++) {
+          //console.log(
+          //data[i].KeyWords[j] + ' ' + event.target.value
+          //);
+          //console.log("event.target.value: " + event.target.value);
+          //console.log("data[i].KeyWords[j].toLowerCase(): " + data[i].KeyWords[j].toLowerCase());
+          if (data[i].KeyWords[j].toLowerCase().includes(event.target.value)) {
+            if (first) {
+              console.log('IN SPECIAL J');
+              finalarray.push(<li>{data[i].Location}</li>);
+              console.log(data[i].Location);
+            }
+            //console.log('something found!!');
+            //console.log("Location: " + data[i].Location);
 
-    for (var i = 0; i < data.length; i++) {
-      for (var j = 0; j < data[i].KeyWords.length; j++) {
-        console.log(
-          data[i].name + ' ' + data[i].KeyWords[j] + ' ' + event.target.value
-        );
-        if (event.target.value.includes(data[i].KeyWords[j].toLowerCase())) {
-          console.log('something found!!');
-          console.log(data[i].Location);
-
-          console.log('FOUND!!!!!' + data[i].Location);
-
-          finalarray.push(<li>{data[i].Location}</li>);
-          console.log(finalarray);
+            //console.log('FOUND!!!!!' + data[i].Location);
+            var founds = false;
+            console.log(checkarray);
+            checkarray.push(data[i].Location);
+            console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII');
+            for (var k = 0; k < checkarray.length; k++) {
+              //console.log("IN FOOOR");
+              if (checkarray[k] === data[i].Location) {
+                console.log('IN IF');
+              } else {
+                founds = true;
+                console.log('IN ELSE');
+              }
+            }
+            console.log(founds);
+            if (founds) {
+              console.log('IN FOUNDS');
+              finalarray.push(<li>{data[i].Location}</li>);
+            }
+            finalarray.push(
+              <ul>
+                <li>{data[i].KeyWords[j]}</li>
+              </ul>
+            );
+            console.log(finalarray);
+            first = false;
+          }
         }
       }
+      if (finalarray.length === 0) {
+        finalarray.push(
+          <ul className="sList">
+            {' '}
+            <li>
+              <h1>Nothing Found</h1>
+            </li>
+          </ul>
+        );
+      }
     }
-    console.log('');
   }
 
   handleSubmit(event) {
@@ -82,13 +124,15 @@ class Map extends Component {
           <img alt="map" className="map" src={map} />
         </div>
 
-        <form onSubmit={this.handleSubmit}>
-          <TextField
-            label="Search"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </form>
+        <center>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              label="Search"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </form>
+        </center>
         <ul>{finalarray}</ul>
       </div>
     );
