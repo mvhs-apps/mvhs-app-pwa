5c6be6939f4351729e049286bfac622cd72f21a1
Mr. Nguyen <mr.lynguyen11@gmail.com>
2022-01-04 18:06:53 -0800

    Update WeatherIcon.js
    Updated to return empty if cannot figure out weather instead of ?.  Broaden search term

--- src/components/Weather/WeatherIcon.js
+++ src/components/Weather/WeatherIcon.js
@@ -37,11 +37,11 @@ const weatherStringToIcon = (weather: string) => {
     return <img className="weather-icon" alt="rain" src={Rainy} />;
   } else if (weather.includes('Shower')) {
     return <img className="weather-icon" alt="rain" src={Rainy} />;
-  } else if (weather.includes('Sunny')) {
+  } else if (weather.includes('Sun')) {
     return <img className="weather-icon" alt="sun" src={WbSunny} />;
-  } else if (weather.includes('Cloudy')) {
+  } else if (weather.includes('Cloud')) {
     return <img className="weather-icon" alt="cloudy" src={WbCloudy} />;
-  } else return <img className="weather-icon" alt="unknown" src={Question} />;
+  } else return Empty;
 };
 
 const WeatherIcon = ({ loading, weather, error }: Props) => {
